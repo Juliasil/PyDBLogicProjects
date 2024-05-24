e53c597672d82dfc73c436f388ab4940c1f2c274
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-05-24 12:14:15 -0300

    Update the .gitignore file

---
+++ matrizes/soma_array.py
@@ -0,0 +1,17 @@
+numlist1 = [1, 2, 3, 4, 5]
+numlist2 = [-1, 1, 0, 5, -5]
+
+listSum = 0
+
+for number in numlist1:
+  listSum += number
+
+
+print(f"Sum of list -> {listSum}")
+
+listSum = 0
+
+for number in numlist2:
+  listSum += number
+
+print(f"Sum of list -> {listSum}")
